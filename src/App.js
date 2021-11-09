1e5dac706c182ffe1ae80ad87e909e37cedcfa9c
msoficaru <mihai@soficaru.com>
2021-11-09 19:10:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/home/home";
-import Details from "./pages/details/details";
-import FourZeroFour from "./pages/fourZeroFour/fourZeroFour";
-import Header from "./components/header/header";
-import { usePersistentStorage } from "./customHooks/usePersistentStorage";
-
- 
- 
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './pages/home/home';
+import Details from './pages/details/details';
+import FourZeroFour from './pages/fourZeroFour/fourZeroFour';
+import Header from './components/header/header';
+import { usePersistentStorage } from './customHooks/usePersistentStorage';
 
 function App() {
+
+  const data = {
+    user: 'Cristi',
+    info: {
+      work: 'developer',
+      sda: 'trainner',
+      hobbies: ['music', 'javascript', 'python', 'books', 'electronics']
+    }
+  };
+
+  const res = usePersistentStorage('get', 'test', data, 'local');
+  console.log(res)
+
   return (
     <div className="App">
-    <Header />
+      <Header />
       <Router>
         <Switch>
-          <Route exact path="/">
+          <Route exact path='/'>
             <Home />
           </Route>
-          <Route exact path="/details/:id">
+          <Route exact path='/details/:id'>
             <Details />
           </Route>
           <Route>
-            <FourZeroFour />
+            <FourZeroFour/>
           </Route>
         </Switch>
-      </Router>
-    </div>
+      </Router >
+    </div >
   );
 }
 
