e7d3b05ce1918e7124831163c73a0d7896c7537b
msoficaru <mihai@soficaru.com>
2021-11-09 19:09:46 +0200

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,86 +1,76 @@
-import React, { useEffect, useState } from "react";
-import { getCoinsMarket } from "../../services/api";
-import TableComponent from "../../components/table/table";
-import Spinner from "react-bootstrap/Spinner";
-import "./home.css";
+import React, { useEffect, useState } from 'react';
+import { getCoinsMarket } from '../../services/api';
+import TableComponent from '../../components/table/table';
+import Spinner from 'react-bootstrap/Spinner';
+import './home.css';
 
 const Home = () => {
-  let initPage = sessionStorage.getItem("page");
-  if (initPage !== null) initPage = Number(initPage);
 
-  const [list, setList] = useState([]);
-  const [error, setError] = useState("");
-  const [tablePageNo, setTablePageNo] = useState(initPage || 1);
-  const [loading, setLoading] = useState(true);
+    let initPage = sessionStorage.getItem('page');
+    if(initPage !== null)initPage = Number(initPage);
+
+    const [list, setList] = useState([]);
+    const [error, setError] = useState('');
+    const [tablePageNo, setTablePageNo] = useState(initPage || 1);
+    const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getCoinsMarketParams = {
-      vs_currency: "eur",
+      vs_currency: 'eur',
       per_page: 10,
-      page: tablePageNo,
+      page: tablePageNo
     };
 
-    async function fetchList() {
-      setLoading(true);
-      const res = await getCoinsMarket(getCoinsMarketParams);
-      console.log(res);
-      if (res.data)
-        setList(
-          res.data.map((k) => {
+    async function fetchList(){
+        setLoading(true);
+        const res = await getCoinsMarket(getCoinsMarketParams);
+        console.log(res)
+        if(res.data)setList(res.data.map(k => {
             return {
-              id: k.id,
-              image: k.image,
-              name: k.name,
-              symbol: k.symbol,
-              current_price: k.current_price,
-              high_24h: k.high_24h,
-              low_24h: k.low_24h,
-            };
-          })
-        );
-      else if (res.error) setError(res.error);
-      setLoading(false);
+                id: k.id,
+                image: k.image,
+                name: k.name,
+                symbol: k.symbol,
+                current_price: k.current_price,
+                high_24h: k.high_24h,
+                low_24h: k.low_24h
+            }
+        }));
+        else if(res.error)setError(res.error)
+        setLoading(false);
     }
-
-    fetchList();
-    sessionStorage.setItem("page", tablePageNo);
-  }, [tablePageNo]);
+    
+    fetchList()
+    sessionStorage.setItem('page', tablePageNo)
+  }, [tablePageNo])
 
   function buildTableHeaderData() {
-    return Object.keys(list[0]).filter((k) => k !== "id");
+      return Object.keys(list[0]).filter(k => k !== 'id');
   }
 
   function handleNextTablePage() {
-    setTablePageNo((prev) => prev + 1);
+    setTablePageNo(prev => prev+1);
   }
 
   function handlePrevTablePage() {
-    if (tablePageNo === 1) return;
-    setTablePageNo((prev) => prev - 1);
+      if(tablePageNo === 1)return
+    setTablePageNo(prev => prev-1);
+
   }
 
-  return (
-    <div className="p-5">
-      {loading ? (
-        <Spinner
-          animation="border"
-          variant="primary"
-          className="spinner-center"
-        />
-      ) : list.length > 0 ? (
-        <TableComponent
-          headerData={buildTableHeaderData()}
-          tableData={list}
-          incrementPageNo={handleNextTablePage}
-          decrementPageNo={handlePrevTablePage}
-          decrementButtonDisable={tablePageNo === 1}
-          pageNo={tablePageNo}
-        />
-      ) : (
-        <div style={{ color: "red" }}>{error}</div>
-      )}
-    </div>
-  );
-};
+    return(
+        <div className="p-5">
+            {loading ? <Spinner animation="border" variant="primary" className="spinner-center"/> 
+            :
+            list.length > 0 ? <TableComponent 
+            headerData={buildTableHeaderData()} 
+            tableData={list}
+            incrementPageNo={handleNextTablePage}
+            decrementPageNo={handlePrevTablePage}
+            decrementButtonDisable={tablePageNo === 1}
+            pageNo={tablePageNo}/> : <div style={{color: 'red'}}>{error}</div>}
+        </div>
+    )
+}
 
 export default Home;
