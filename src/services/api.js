681bb86c0c6153abb149ab174c114b6b9c52dab1
msoficaru <mihai@soficaru.com>
2021-11-09 19:10:16 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,63 +1,68 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "https://api.coingecko.com/api/v3",
-  headers: { Accept: "application/json" },
+    baseURL: 'https://api.coingecko.com/api/v3',
+    headers: {'Accept': 'application/json'}
 });
 
 const buildParams = (params) => {
-  if (params === undefined) return "";
-  const arr = [];
-  Object.keys(params).forEach((k) => {
-    if (params[k] !== undefined) arr.push(`${k}=${params[k]}`);
-  });
-  return "?" + arr.join("&");
-};
+    if(params === undefined)return ''
+    const arr = [];
+    Object.keys(params).forEach(k => {
+        if(params[k] !== undefined)arr.push(`${k}=${params[k]}`)
+    });
+    return '?'+arr.join('&');
+}
 
 //TODO reqParams to be array
 const validateParams = (params, reqParams) => {
-  let isValid = params !== undefined;
-  if (params) {
-    const paramsArray = Object.keys(params);
-    reqParams.forEach((rp) => {
-      if (!paramsArray.includes(rp)) {
-        isValid = false;
-      }
-    });
-  }
-  return isValid;
-};
+    let isValid = params !== undefined;
+    if(params){
+        const paramsArray = Object.keys(params);
+        reqParams.forEach((rp) => {
+            if(!paramsArray.includes(rp)){
+                isValid = false;
+            }
+        })
+    }
+    return isValid;
+}
 
 export const getCoinsMarket = async (params) => {
-  const isValid = validateParams(params, ["vs_currency"]);
+    const isValid = validateParams(params, ['vs_currency']);
 
-  if (isValid) {
-    const parsedParams = buildParams(params);
-    try {
-      const res = await instance.get(`/coins/markets${parsedParams}`);
-      return { data: res.data };
-    } catch (error) {
-      return { error };
+    if(isValid){
+        const parsedParams = buildParams(params)
+        try{
+            const res = await instance.get(`/coins/markets${parsedParams}`);
+            return { data: res.data };
+        }
+        catch(error){
+            return { error }
+        }       
+    }  
+    else {
+        return {
+            error: 'Invalid call parameters'
+        }
     }
-  } else {
-    return {
-      error: "Invalid call parameters",
-    };
-  }
-};
+}
 
 export const getCoinDetails = async (subUrl, params) => {
-  if (subUrl) {
-    const parsedParams = buildParams(params);
-    try {
-      const res = await instance.get(`/coins/${subUrl}${parsedParams}`);
-      return { data: res.data };
-    } catch (error) {
-      return { error };
+
+    if(subUrl){
+        const parsedParams = buildParams(params)
+        try{
+            const res = await instance.get(`/coins/${subUrl}${parsedParams}`);
+            return { data: res.data };
+        }
+        catch(error){
+            return { error }
+        }       
+    }  
+    else {
+        return {
+            error: 'Invalid call parameters'
+        }
     }
-  } else {
-    return {
-      error: "Invalid call parameters",
-    };
-  }
-};
+}
