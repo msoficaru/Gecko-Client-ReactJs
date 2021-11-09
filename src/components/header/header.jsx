ab0eb33c88717d208eb0a9d993324db60a1121f7
msoficaru <mihai@soficaru.com>
2021-11-09 19:06:28 +0200

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,28 +1,22 @@
-import React from "react";
-import { Navbar } from "react-bootstrap";
-import "./header.css";
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import './header.css';
 
 const Header = () => {
-  return (
-    <Navbar bg="dark" variant="dark" className="d-flex justify-content-between">
-      <Navbar.Brand className="d-flex">
-        <img
-          alt=""
-          src="/logo512.png"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />
-        <div className="ml-2">Gecko Client</div>
-      </Navbar.Brand>
-      <a
-        href="https://www.coingecko.com/api/documentations/v3#/"
-        className="header-small-info"
-      >
-        Build with Coin Gecko API
-      </a>
-    </Navbar>
-  );
-};
+    return (
+        <Navbar bg="dark" variant="dark" className="d-flex justify-content-between">
+        <Navbar.Brand className="d-flex">
+          <img
+            alt=""
+            src="/logo512.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          /><div className="ml-2">Gecko Client</div>
+        </Navbar.Brand>
+        <a href="https://www.coingecko.com/api/documentations/v3#/" className="header-small-info">Build with Coin Gecko API</a>
+      </Navbar>
+    )
+}
 
 export default Header;
