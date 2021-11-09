d5047d7581441509f5a7497a61b29ee2f9fc585e
msoficaru <mihai@soficaru.com>
2021-11-09 19:05:38 +0200

    Update header.test.js

--- src/__test__/header.test.js
+++ src/__test__/header.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from '../components/header/header';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect/';
+import '@testing-library/jest-dom/extend-expect';
 
 
-describe("this should render the header component corect", () =>{
+
+test("This should render the Header component corect", () => {
     const { getByText } = render(<Header />);
-    expect(getByText("Gecko Client")).toBeInTheDocument();
+    expect(getByText('Gecko Client')).toBeInTheDocument();
 })
